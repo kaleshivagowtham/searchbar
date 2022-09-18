bb15d1507f0133b55d9b59adad27883f87735bb2
Shiva_Gowtham_Kale <kaleshivagowtham@gmail.com>
2022-09-18 21:18:58 +0530

    basic UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import classes from './App.module.css';
+import srchIcon from './search-icon.svg';
+import data from './data.json';
 
-function App() {
+export default function App({props}) {
+
+  const [srchInp , setSrchInp] = useState();
+
+  const srchHandler = (e) => {
+    setSrchInp(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.wholeCont}>
+      <input className={classes.srchInput} onChange={srchHandler} style={{}} />
+      <button className={classes.srchIconCont}>
+        <img src={srchIcon} className={classes.srchIcon} style={{}} />
+      </button>
+      <div className={classes.srchDD}>
+          {
+            data.filter((val)=>{
+              if(srchInp === ""){
+                return null;
+              }
+              else if(srchInp.toLowerCase() === "all"){
+                return val;
+              }
+              else if(val.name.toLowerCase().includes(srchInp.toLowerCase())){
+                return val;
+              }
+            }).map((val , key) => {
+              return (
+                <p className={classes.ddList}>{val.name}</p>
+              )
+            })}
+      </div>
     </div>
   );
 }
-
-export default App;

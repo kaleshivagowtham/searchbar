1f01556d5579010719753f9fdd3c7d1998131abe
Shiva_Gowtham_Kale <kaleshivagowtham@gmail.com>
2023-04-07 13:39:41 +0530

    testing remaining

--- src/App.js
+++ src/App.js
@@ -1,38 +1,68 @@
 import React, { useState } from 'react';
 import classes from './App.module.css';
-import srchIcon from './search-icon.svg';
-import info from './data.json';
+import srchIconBlack from './search-icon-black.png';
+import srchIconWhite from './search-icon-white.png';
 
-export default function App({props}) {
+
+export default function App(props) {
 
   const [srchInp , setSrchInp] = useState('');
+  // const [info , setInfo] = useState([{name : "" , link : ""}]);
 
   const srchHandler = (e) => {
     setSrchInp(e.target.value);
   }
 
   return (
-    <div className={classes.wholeCont}>
-      <input className={classes.srchInput} onChange={srchHandler} style={{}} />
-      <button className={classes.srchIconCont}>
-        <img src={srchIcon} className={classes.srchIcon} style={{}} />
-      </button>
-      <div className={classes.srchDD}>
+    <div className={classes.wholeCont} 
+      style={
+      {
+        width : props.SearchBarWidth ? props.SearchBarWidth : "80vh" ,
+        height : props.SearchBarHeight ? props.SearchBarHeight : "7vh" ,
+        fontFamily : props.searchFontStyle ? props.searchFontStyle : "'Open Sans', sans-serif" , 
+        border : props.borderLine ? props.borderLine : "1px solid rgb(219, 219, 219)" ,
+      }}>
+
+        <input className={classes.srchInput} onChange={srchHandler} style={{}} />
+
+        <button className={classes.srchIconCont}
+          style={{ backgroundColor : props.searchBackgroundColor ? props.searchBackgroundColor : "#FBC02D"}}>
+
+            <img src={props.searchIconColor === "black" ? srchIconBlack :srchIconWhite} 
+                alt="search Icon" className={classes.srchIcon} />
+
+        </button>
+
+      { srchInp && <div className={classes.srchDD}>
           {
-            info.filter((val)=>{
-              if(srchInp === '')
-                return null;
-              else if (srchInp.toLowerCase() === 'all')
-                return val;
-              else if (val.name.toLowerCase().includes(srchInp.toLowerCase()))
-                return val;
-            }).map((item) => {
-              return (
-                <a href={item.link} target='_blank'><p className={classes.ddList}>{item.name}</p></a>
-              )
-            })
+              (props.data).filter((val)=>{
+                if(srchInp === '')
+                  return null;
+                else if (srchInp.toLowerCase() === 'all')
+                  return val;
+                else if (val.name.toLowerCase().includes(srchInp.toLowerCase()))
+                  return val;
+              }).map((item) => {
+                return (
+                  <a href={item.link} className={classes.ddList} target='_blank' rel="noreferrer">
+                    <p style={{margin:"0"}}>{item.name}</p>
+                  </a>
+                )
+              })
           }
       </div>
+      }
+
     </div>
   );
 }
+
+/*
+  props.searchBackgroundColor -> background color of search button
+  props.searchFontStyle -> font family
+  props.borderLine -> "line width   line style(solid/dashed)   line color "
+  props.searchIconColor -> white/black
+  props.data -> pass data as an object
+  props.SearchBarWidth -> width of the whole component
+  props.SearchBarHeight -> height of the whole component without dropdown
+*/
